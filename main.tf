e9d72dd21cb2c1c578e8f44898e3c43434c69f4f
Kutchrlapati, Sai Manoj Varma (Contractor) <saimanojvarma.kutchrlapati+mars@effem.com>
2026-01-21 15:51:41 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source       = "./modules/vpc"
   project_name = var.project_name
-  vpc_cidr = var.vpc_cidr
-  public_cidr = var.public_subnet_cidr
+  vpc_cidr     = var.vpc_cidr
+  public_cidr  = var.public_subnet_cidr
   private_cidr = var.private_subnet_cidr
-  az = var.az
+  az           = var.az
 }
