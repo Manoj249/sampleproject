850c951b02372d340a86d77a75726a0d939b12db
Kutchrlapati, Sai Manoj Varma (Contractor) <saimanojvarma.kutchrlapati+mars@effem.com>
2026-01-23 14:48:32 +0530

    updated

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "iam_ec2" {
   source = "./modules/iam-ec2"
   project_name = var.project_name
   vpc_id = module.vpc.vpc_id
-  subnet_id = module.vpc.public_subnet_id
+  subnet_id = module.vpc.public_subnet_cidr
   my_ip = var.my_ip
   ami_id = var.ami_id
   instance_type = var.instance_type
