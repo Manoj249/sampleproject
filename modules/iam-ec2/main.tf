ee0474a4c4e2098a7ee76963fc1f9e4734ae235c
Kutchrlapati, Sai Manoj Varma (Contractor) <saimanojvarma.kutchrlapati+mars@effem.com>
2026-01-23 18:57:48 +0530

    committed

--- modules/iam-ec2/main.tf
+++ modules/iam-ec2/main.tf
@@ -46,7 +46,7 @@ resource "aws_iam_role" "ec2_role" {
 resource "aws_iam_role_policy_attachment" "s3_access" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-}
+} 
 
 # Instance Profile
 resource "aws_iam_instance_profile" "ec2_profile" {
