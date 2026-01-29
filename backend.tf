f84d1906018d00974820556c33b204042369780e
Kutchrlapati, Sai Manoj Varma (Contractor) <saimanojvarma.kutchrlapati+mars@effem.com>
2026-01-29 12:00:04 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-demo-week3-state-bucket"
+    bucket = "terraform-demo-week3-state-bucket1"
     key = "terraform/state/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
